bba91aac503feb8453fab81b5a8627e4b795fc97
Avi Cavale <avi@shippable.com>
2017-02-19 18:25:25 +0530

    upgrading build ami sh script

--- awsBuildAMI/basePack.sh
+++ awsBuildAMI/basePack.sh
@@ -1,99 +1,86 @@
 #!/bin/bash -e
 
-export PK_INSALL_LOCATION=/opt
-export PK_VERSION=0.11.0
-export PK_FILENAME=packer_"$PK_VERSION"_linux_amd64.zip
+export CURR_JOB="build_baseami"
 export RES_AWS_CREDS="aws_creds"
-export REPO_RESOURCE_NAME="auto_repo"
-export RES_PARAMS=$1
+export RES_PARAMS="bldami_params"
+export RES_REPO="auto_repo"
 
-echo "RES_PARAMS=$RES_PARAMS"
+# since resources here have dashes Shippable replaces them and UPPER cases them
+export CURR_JOB_UP=$(echo $CURR_JOB | awk '{print toupper($0)}')
+export CURR_JOB_STATE=$(eval echo "$"$CURR_JOB_UP"_STATE")
 
-setup_ssh(){
-  eval `ssh-agent -s`
-  ps -eaf | grep ssh
-  ls -al ~/.ssh/
-  which ssh-agent
-}
+# since resources here have dashes Shippable replaces them and UPPER cases them
+export RES_PARAMS_UP=$(echo $RES_PARAMS | awk '{print toupper($0)}')
+export RES_PARAMS_STR=$RES_PARAMS_UP"_PARAMS"
 
-setup_keys() {
-  pushd /build/IN/$RES_AWS_CREDS
-  echo "-----------------------------------"
+# Now get AWS keys
+export RES_AWS_CREDS_UP=$(echo $RES_AWS_CREDS | awk '{print toupper($0)}')
+export RES_AWS_CREDS_INT=$RES_AWS_CREDS_UP"_INTEGRATION"
 
-  echo "setting AWS keys"
-  echo "-----------------------------------"
-  . integration.env
-  popd
-}
+# set the repo path
+export RES_REPO_UP=$(echo $RES_REPO | awk '{print toupper($0)}')
+export RES_REPO_STATE=$(eval echo "$"$RES_REPO_UP"_STATE")
 
-setup_params(){
-  pushd ./IN/$RES_PARAMS
-  export REGION=$(jq -r '.version.propertyBag.params.REGION' version.json)
-  export VPC_ID=$(jq -r '.version.propertyBag.params.VPC_ID' version.json)
-  export SUBNET_ID=$(jq -r '.version.propertyBag.params.SUBNET_ID' version.json)
-  export SECURITY_GROUP_ID=$(jq -r '.version.propertyBag.params.SECURITY_GROUP_ID' version.json)
-  export SOURCE_AMI=$(jq -r '.version.propertyBag.params.SOURCE_AMI' version.json)
+set_context(){
+  # now get all the parameters for ami location
+  export REGION=$(eval echo "$"$RES_PARAMS_STR"_REGION")
+  export VPC_ID=$(eval echo "$"$RES_PARAMS_STR"_VPC_ID")
+  export SUBNET_ID=$(eval echo "$"$RES_PARAMS_STR"_SUBNET_ID")
+  export SECURITY_GROUP_ID=$(eval echo "$"$RES_PARAMS_STR"_SECURITY_GROUP_ID")
+  export SOURCE_AMI=$(eval echo "$"$RES_PARAMS_STR"_SOURCE_AMI")
+
+  # now get the AWS keys
+  export AWS_ACCESS_KEY_ID=$(eval echo "$"$RES_AWS_CREDS_INT"_AWS_ACCESS_KEY_ID")
+  export AWS_SECRET_ACCESS_KEY=$(eval echo "$"$RES_AWS_CREDS_INT"_AWS_SECRET_ACCESS_KEY")
+
+  echo "CURR_JOB=$CURR_JOB"
+  echo "RES_AWS_CREDS=$RES_AWS_CREDS"
+  echo "RES_PARAMS=$RES_PARAMS"
+  echo "RES_REPO=$RES_REPO"
+
+  echo "CURR_JOB_STATE=$CURR_JOB_STATE"
+  echo "RES_AWS_CREDS_INT=$RES_AWS_CREDS_INT"
+  echo "RES_PARAMS_STR=$RES_PARAMS_STR"
+  echo "RES_REPO_STATE=$RES_REPO_STATE"
 
   echo "SOURCE_AMI=$SOURCE_AMI"
   echo "VPC_ID=$VPC_ID"
   echo "REGION=$REGION"
   echo "SUBNET_ID=$SUBNET_ID"
   echo "SECURITY_GROUP_ID=$SECURITY_GROUP_ID"
-
-  popd
-}
-
-install_packer() {
-  pushd $PK_INSALL_LOCATION
-  echo "Fetching packer"
-  echo "-----------------------------------"
-
-  rm -rf $PK_INSALL_LOCATION/packer
-  mkdir -p $PK_INSALL_LOCATION/packer
-
-  wget -q https://releases.hashicorp.com/packer/$PK_VERSION/"$PK_FILENAME"
-  apt-get install unzip
-  unzip -o $PK_FILENAME -d $PK_INSALL_LOCATION/packer
-  export PATH=$PATH:$PK_INSALL_LOCATION/packer
-  echo "downloaded packer successfully"
-  echo "-----------------------------------"
-
-  local pk_version=$(packer version)
-  echo "Packer version: $pk_version"
-  popd
+  echo "AWS_ACCESS_KEY_ID=${#AWS_ACCESS_KEY_ID}" #print only length not value
+  echo "AWS_SECRET_ACCESS_KEY=${#AWS_SECRET_ACCESS_KEY}" #print only length not value
 }
 
 build_ami() {
-  pushd /build/IN/$REPO_RESOURCE_NAME/gitRepo/awsBuildAMI
-  echo "-----------------------------------"
-
+  pushd "$RES_REPO_STATE/awsBuildAMI"
   echo "validating AMI template"
   echo "-----------------------------------"
   packer validate baseAMI.json
   echo "building AMI"
   echo "-----------------------------------"
 
-  packer build -machine-readable -var 'aws_access_key='$aws_access_key_id \
-    -var 'aws_secret_key='$aws_secret_access_key \
-    -var 'REGION='$REGION \
-    -var 'VPC_ID='$VPC_ID \
-    -var 'SUBNET_ID='$SUBNET_ID \
-    -var 'SECURITY_GROUP_ID='$SECURITY_GROUP_ID \
-    -var 'SOURCE_AMI='$SOURCE_AMI \
+  packer build -machine-readable -var aws_access_key=$aws_access_key_id \
+    -var aws_secret_key=$aws_secret_access_key \
+    -var REGION=$REGION \
+    -var VPC_ID=$VPC_ID \
+    -var SUBNET_ID=$SUBNET_ID \
+    -var SECURITY_GROUP_ID=$SECURITY_GROUP_ID \
+    -var SOURCE_AMI=$SOURCE_AMI \
     baseAMI.json 2>&1 | tee output.txt
 
     #this is to get the ami from output
-    echo AMI_ID=$(cat output.txt | awk -F, '$0 ~/artifact,0,id/ {print $6}' \
-    | cut -d':' -f 2) > /build/state/AMI_ID.txt
-    cat /build/state/AMI_ID.txt
+    echo versionName=$(cat output.txt | awk -F, '$0 ~/artifact,0,id/ {print $6}' \
+    | cut -d':' -f 2) > /build/state/$CURR_JOB.env
+
   popd
 }
 
 main() {
-  setup_ssh
-  setup_keys
-  setup_params
-  install_packer
+  eval `ssh-agent -s`
+  which ssh-agent
+
+  set_context
   build_ami
 }
 
