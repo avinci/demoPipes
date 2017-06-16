e4314f5b90e0b712695ce6b193031b973385a327
Avi Cavale <avi@shippable.com>
2017-06-16 09:30:05 -0500

    parameterizing network

--- outputNetwork.sh
+++ outputNetwork.sh
@@ -2,7 +2,7 @@
 set -o pipefail
 
 export CURR_JOB="aws_infra_setup"
-export RES_AWS_CREDS="test_creds"
+export RES_NET_PARAMS="network_params"
 
 # make this come from terraform
 export VPC_ID="vpc-1032db76"
@@ -19,14 +19,11 @@ set_network_params(){
   echo "CURR_JOB=$CURR_JOB"
   echo "RES_AWS_CREDS=$RES_AWS_CREDS"
 
-  echo "CURR_JOB_STATE=$CURR_JOB_STATE"
-  echo "JOB_STATE=$JOB_STATE"
-
-  echo "VPC_ID=$VPC_ID" >> "$JOB_STATE/$CURR_JOB.env"
-  echo "SUBNET_ID=$SUBNET_ID" >> "$JOB_STATE/$CURR_JOB.env"
-  echo "SECURITY_GROUP_ID=$SECURITY_GROUP_ID" >> "$JOB_STATE/$CURR_JOB.env"
-  echo "REGION=$REGION" >> "$JOB_STATE/$CURR_JOB.env"
-  cat "$JOB_STATE/$CURR_JOB.env"
+  echo "VPC_ID=$VPC_ID" >> "$JOB_STATE/$RES_NET_PARAMS.env"
+  echo "SUBNET_ID=$SUBNET_ID" >> "$JOB_STATE/$RES_NET_PARAMS.env"
+  echo "SECURITY_GROUP_ID=$SECURITY_GROUP_ID" >> "$JOB_STATE/$RES_NET_PARAMS.env"
+  echo "REGION=$REGION" >> "$JOB_STATE/$RES_NET_PARAMS.env"
+  cat "$JOB_STATE/$RES_NET_PARAMS.env"
 }
 
 main() {
