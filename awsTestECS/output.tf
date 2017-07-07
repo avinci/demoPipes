4886079faf127f7a57f7e883eb437f519d5d8898
Avi Cavale <avi@shippable.com>
2017-07-06 18:37:22 -0700

    testing test prov

--- awsTestECS/output.tf
+++ awsTestECS/output.tf
@@ -1,9 +1,9 @@
 output "test_ecs_ins_0_ip" {
-  value = "${aws_instance.testECSIns.0.id}"
+  value = "${aws_instance.testECSIns.0.public_ip}"
 }
 
 output "test_ecs_ins_1_ip" {
-  value = "${aws_instance.testECSIns.1.id}"
+  value = "${aws_instance.testECSIns.1.public_ip}"
 }
 
 output "test_ecs_cluster_id" {
