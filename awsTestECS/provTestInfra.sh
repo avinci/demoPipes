7a880c0c2b66a85a90973736028a64f74d996e72
Avi Cavale <avi@shippable.com>
2017-07-09 09:15:47 -0700

    testing test prov

--- awsTestECS/provTestInfra.sh
+++ awsTestECS/provTestInfra.sh
@@ -9,7 +9,6 @@ export OUT_RES_SET="test_env_ecs"
 
 export RES_REPO="auto_repo"
 export RES_AWS_CREDS="aws_creds"
-export RES_AWS_PEM="aws_pem"
 export TF_STATEFILE="terraform.tfstate"
 
 # get the path where gitRepo code is available
@@ -33,7 +32,6 @@ set_context(){
   echo "CURR_JOB_CONTEXT=$CURR_JOB_CONTEXT"
   echo "RES_REPO=$RES_REPO"
   echo "RES_AWS_CREDS=$RES_AWS_CREDS"
-  echo "RES_AWS_PEM=$RES_AWS_PEM"
   echo "RES_REPO_CONTEXT=$RES_REPO_CONTEXT"
 
   echo "AWS_ACCESS_KEY_ID=${#AWS_ACCESS_KEY_ID}" #print only length not value
@@ -42,13 +40,8 @@ set_context(){
   # This restores the terraform state file
   ship_copy_file_from_resource_state $STATE_RES $TF_STATEFILE .
 
-  # This gets the PEM key for SSH into the machines
-  ship_get_resource_integration_value $RES_AWS_PEM key > demo-key.pem
-  chmod 600 demo-key.pem
-
   # now setup the variables based on context
   # naming the file terraform.tfvars makes terraform automatically load it
-
   echo "aws_access_key_id = \"$AWS_ACCESS_KEY_ID\"" > terraform.tfvars
   echo "aws_secret_access_key = \"$AWS_SECRET_ACCESS_KEY\"" >> terraform.tfvars
   echo "region = \"$REGION\"" >> terraform.tfvars
