0eb6d1acdf671da74aadfb95c66cfb5648f17ec7
Avi Cavale <avi@shippable.com>
2017-08-15 19:57:49 +0530

    upgrading to new utility functions

--- awsSetupIAM/provNetwork.sh
+++ awsSetupIAM/provNetwork.sh
@@ -1,88 +1,42 @@
 #!/bin/bash -e
 
+# since we are using params resource `net_conf` as an IN all the params are set
+# automatically. $REGION, #VPC settings that are used in tfvars file
+
 export ACTION=$1
 export CURR_JOB_CONTEXT="awsSetupIAM"
+export RES_REPO="auto_repo"
+export RES_AWS_CREDS="aws_creds"
+
 export STATE_RES="net_tf_state"
-export RES_CONF="net_conf"
+export TF_STATEFILE="terraform.tfstate"
 export OUT_AMI_VPC="ami_vpc_conf"
 export OUT_TEST_VPC="test_vpc_conf"
 export OUT_PROD_VPC="prod_vpc_conf"
 
-export RES_REPO="auto_repo"
-export RES_AWS_CREDS="aws_creds"
-export RES_AWS_PEM="aws_pem"
-export TF_STATEFILE="terraform.tfstate"
-
 # get the path where gitRepo code is available
-export RES_REPO_STATE=$(ship_resource_get_state $RES_REPO)
+export RES_REPO_STATE=$(shipctl get_resource_state $RES_REPO)
 export RES_REPO_CONTEXT="$RES_REPO_STATE/$CURR_JOB_CONTEXT"
 
 # Now get AWS keys
-export AWS_ACCESS_KEY_ID=$(ship_resource_get_integration $RES_AWS_CREDS aws_access_key_id)
-export AWS_SECRET_ACCESS_KEY=$(ship_resource_get_integration $RES_AWS_CREDS aws_secret_access_key)
-
-# Now get all VPC settings
-export REGION=$(ship_resource_get_param $RES_CONF REGION)
-export AMI_VPC=$(ship_resource_get_param $RES_CONF AMI_VPC)
-export AMI_NETWORK_CIDR=$(ship_resource_get_param $RES_CONF AMI_NETWORK_CIDR)
-export AMI_PUBLIC_CIDR=$(ship_resource_get_param $RES_CONF AMI_PUBLIC_CIDR)
-export TEST_VPC=$(ship_resource_get_param $RES_CONF TEST_VPC)
-export TEST_NETWORK_CIDR=$(ship_resource_get_param $RES_CONF TEST_NETWORK_CIDR)
-export TEST_PUBLIC_CIDR=$(ship_resource_get_param $RES_CONF TEST_PUBLIC_CIDR)
-export PROD_VPC=$(ship_resource_get_param $RES_CONF PROD_VPC)
-export PROD_NETWORK_CIDR=$(ship_resource_get_param $RES_CONF PROD_NETWORK_CIDR)
-export PROD_PUBLIC_CIDR=$(ship_resource_get_param $RES_CONF PROD_PUBLIC_CIDR)
+export AWS_ACCESS_KEY_ID=$(shipctl get_integration_resource_field $RES_AWS_CREDS aws_access_key_id)
+export AWS_SECRET_ACCESS_KEY=$(shipctl get_integration_resource_field $RES_AWS_CREDS aws_secret_access_key)
 
 set_context(){
-  pushd $RES_REPO_CONTEXT
-
-  echo "CURR_JOB_CONTEXT=$CURR_JOB_CONTEXT"
-  echo "RES_REPO=$RES_REPO"
-  echo "RES_AWS_CREDS=$RES_AWS_CREDS"
-  echo "RES_AWS_PEM=$RES_AWS_PEM"
-  echo "RES_REPO_CONTEXT=$RES_REPO_CONTEXT"
-
-  echo "AWS_ACCESS_KEY_ID=${#AWS_ACCESS_KEY_ID}" #print only length not value
-  echo "AWS_SECRET_ACCESS_KEY=${#AWS_SECRET_ACCESS_KEY}" #print only length not value
-
   # This restores the terraform state file
-  ship_resource_copy_file_from_state $STATE_RES $TF_STATEFILE .
-
-  # This gets the PEM key for SSH into the machines
-  ship_resource_get_integration $RES_AWS_PEM key > demo-key.pem
-  chmod 600 demo-key.pem
+  shipctl copy_resource_file_from_state $STATE_RES $TF_STATEFILE
 
   # now setup the variables based on context
   # naming the file terraform.tfvars makes terraform automatically load it
-
-  echo "aws_access_key_id = \"$AWS_ACCESS_KEY_ID\"" > terraform.tfvars
-  echo "aws_secret_access_key = \"$AWS_SECRET_ACCESS_KEY\"" >> terraform.tfvars
-  echo "region = \"$REGION\"" >> terraform.tfvars
-  echo "ami_vpc = \"$AMI_VPC\"" >> terraform.tfvars
-  echo "ami_network_cidr = \"$AMI_NETWORK_CIDR\"" >> terraform.tfvars
-  echo "ami_public_cidr = \"$AMI_PUBLIC_CIDR\"" >> terraform.tfvars
-  echo "test_vpc = \"$TEST_VPC\"" >> terraform.tfvars
-  echo "test_network_cidr = \"$TEST_NETWORK_CIDR\"" >> terraform.tfvars
-  echo "test_public_cidr = \"$TEST_PUBLIC_CIDR\"" >> terraform.tfvars
-  echo "prod_vpc = \"$PROD_VPC\"" >> terraform.tfvars
-  echo "prod_network_cidr = \"$PROD_NETWORK_CIDR\"" >> terraform.tfvars
-  echo "prod_public_cidr = \"$PROD_PUBLIC_CIDR\"" >> terraform.tfvars
-
-  popd
+  shipctl replace terraform.tfvars
 }
 
 destroy_changes() {
-  pushd $RES_REPO_CONTEXT
-
   echo "----------------  Destroy changes  -------------------"
   terraform destroy -force
-
-  popd
 }
 
 apply_changes() {
-  pushd $RES_REPO_CONTEXT
-
   echo "----------------  Planning changes  -------------------"
   terraform plan
 
@@ -90,41 +44,39 @@ apply_changes() {
   terraform apply
 
   #output AMI VPC
-  ship_resource_post_state $OUT_AMI_VPC versionName \
+  shipctl post_resource_state $OUT_AMI_VPC versionName \
     "Version from build $BUILD_NUMBER"
-  ship_resource_put_state $OUT_AMI_VPC REGION $REGION
-  ship_resource_put_state $OUT_AMI_VPC BASE_ECS_AMI \
+  shipctl put_resource_state $OUT_AMI_VPC REGION $REGION
+  shipctl put_resource_state $OUT_AMI_VPC BASE_ECS_AMI \
     $(terraform output base_ecs_ami)
-  ship_resource_put_state $OUT_AMI_VPC AMI_VPC_ID \
+  shipctl put_resource_state $OUT_AMI_VPC AMI_VPC_ID \
     $(terraform output ami_vpc_id)
-  ship_resource_put_state $OUT_AMI_VPC AMI_PUBLIC_SN_ID \
+  shipctl put_resource_state $OUT_AMI_VPC AMI_PUBLIC_SN_ID \
     $(terraform output ami_public_sn_id)
-  ship_resource_put_state $OUT_AMI_VPC AMI_PUBLIC_SG_ID \
+  shipctl put_resource_state $OUT_AMI_VPC AMI_PUBLIC_SG_ID \
     $(terraform output ami_public_sg_id)
 
   #output TEST VPC
-  ship_resource_post_state $OUT_TEST_VPC versionName \
+  shipctl post_resource_state $OUT_TEST_VPC versionName \
     "Version from build $BUILD_NUMBER"
-  ship_resource_put_state $OUT_TEST_VPC REGION $REGION
-  ship_resource_put_state $OUT_TEST_VPC TEST_VPC_ID \
+  shipctl put_resource_state $OUT_TEST_VPC REGION $REGION
+  shipctl put_resource_state $OUT_TEST_VPC TEST_VPC_ID \
     $(terraform output test_vpc_id)
-  ship_resource_put_state $OUT_TEST_VPC TEST_PUBLIC_SN_ID \
+  shipctl put_resource_state $OUT_TEST_VPC TEST_PUBLIC_SN_ID \
     $(terraform output test_public_sn_id)
-  ship_resource_put_state $OUT_TEST_VPC TEST_PUBLIC_SG_ID \
+  shipctl put_resource_state $OUT_TEST_VPC TEST_PUBLIC_SG_ID \
     $(terraform output test_public_sg_id)
 
   #output PROD VPC
-  ship_resource_post_state $OUT_PROD_VPC versionName \
+  shipctl post_resource_state $OUT_PROD_VPC versionName \
     "Version from build $BUILD_NUMBER"
-  ship_resource_put_state $OUT_PROD_VPC REGION $REGION
-  ship_resource_put_state $OUT_PROD_VPC PROD_VPC_ID \
+  shipctl put_resource_state $OUT_PROD_VPC REGION $REGION
+  shipctl put_resource_state $OUT_PROD_VPC PROD_VPC_ID \
     $(terraform output test_vpc_id)
-  ship_resource_put_state $OUT_PROD_VPC PROD_PUBLIC_SN_ID \
+  shipctl put_resource_state $OUT_PROD_VPC PROD_PUBLIC_SN_ID \
     $(terraform output test_public_sn_id)
-  ship_resource_put_state $OUT_PROD_VPC PROD_PUBLIC_SG_ID \
+  shipctl put_resource_state $OUT_PROD_VPC PROD_PUBLIC_SG_ID \
     $(terraform output test_public_sg_id)
-
-  popd
 }
 
 main() {
@@ -132,6 +84,8 @@ main() {
   eval `ssh-agent -s`
   ps -eaf | grep ssh
   which ssh-agent
+
+  pushd $RES_REPO_CONTEXT
 
   set_context
 
@@ -142,6 +96,8 @@ main() {
   if [ $ACTION = "destroy" ]; then
     destroy_changes
   fi
+
+  popd
 }
 
 main
